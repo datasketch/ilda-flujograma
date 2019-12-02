616f1fb919e7dd1a4931809e196f0d4256bdee86
David Daza <03dazal@gmail.com>
2019-12-02 10:07:59 -0500

    add support to mobile

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,6 @@
 const svg = document.querySelector('svg');
 const indicadores = document.getElementById('indicadores');
+let touchstart = null;
 
 function resize() {
   svg.setAttribute('width', window.innerWidth);
@@ -18,23 +19,43 @@ function setViewBox(values) {
 
 function handleWheelEvent(event) {
   const [minX, minY, width, height] = getViewBox();
-  let shiftX = event.deltaX + minX;
-  if (shiftX < 0) {
-    shiftX = 0;
+  let shift = event.deltaY + minX;
+  if (shift < 0) {
+    shift = 0;
   }
-  if (shiftX > (width - window.innerWidth)) {
-    shiftX = (width - window.innerWidth);
+  if (shift > (width - window.innerWidth)) {
+    shift = (width - window.innerWidth);
   }
-  setViewBox([shiftX, minY, width, height]);
+  setViewBox([shift, minY, width, height]);
+}
+
+function handleTouchStart(event) {
+  touchstart = event.touches[0].clientX;
+}
+
+function handleTouchMove(event) {
+  console.log(event.touches[0].pageX);
+  var touchmove = event.changedTouches[0].clientX;
+  var delta = touchstart - touchmove;
+  const [minX, minY, width, height] = getViewBox();
+  let shift = minX + (delta < 0 ? -50 : 50);
+  if (shift < 0) {
+    shift = 0;
+  }
+  if (shift > (width - window.innerWidth)) {
+    shift = (width - window.innerWidth);
+  }
+  setViewBox([shift, minY, width, height]);
 }
 
 indicadores.addEventListener('click', function (event) {
   location.replace('/indicadores.svg');
 });
 
-window.addEventListener('resize', resize);
-window.addEventListener('wheel', handleWheelEvent);
-
 window.onload = function () {
   resize();
+  window.addEventListener('resize', resize);
+  window.addEventListener('wheel', handleWheelEvent);
+  window.addEventListener('touchstart', handleTouchStart);
+  window.addEventListener('touchmove', handleTouchMove);
 }
